149828267ee8bde56296c4852d196c6a4f59652c
andreluizrcn <andreluizrodriguescastro@gmail.com>
2026-06-16 17:30:39 -0300

    Chapter 2: declared Abstract Syntax Tree

--- chapter2/parser.cpp
+++ chapter2/parser.cpp
@@ -1,1 +1,13 @@
 /* ============== CHAPTER 2 - PARSER =================*/
+
+class ExprAST {
+public:
+  virtual ~ExprAST() = default;
+};
+
+class NumberExprAST : public ExprAST {
+  double Val;
+
+public:
+  NumberExprAST(double Val) : Val(Val) {}
+};
