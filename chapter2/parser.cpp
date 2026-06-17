a7c1a9c09f940663e3d66debb6962553f63aaa44
andreluizrcn <andreluizrodriguescastro@gmail.com>
2026-06-17 13:27:22 -0300

    add more important classes to parser

--- chapter2/parser.cpp
+++ chapter2/parser.cpp
@@ -1,13 +1,36 @@
 /* ============== CHAPTER 2 - PARSER =================*/
 
+#include <memory>
+#include <string>
+#include <utility>
+
 class ExprAST {
 public:
   virtual ~ExprAST() = default;
 };
 
-class NumberExprAST : public ExprAST {
+class NumberExprAST : public ExprAST { // inherit from ExprAST to num
   double Val;
 
 public:
-  NumberExprAST(double Val) : Val(Val) {}
+  NumberExprAST(double V) : Val(V) {} // constructor + init
+};
+
+class VariableExprAST : public ExprAST {
+  std::string Name;
+
+  /* More efficient than just copying the data in memory and pushing it on the
+   * stack. Saves memory and processing time. */
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+class BinaryExprAST : public ExprAST {
+  char Op; // + - * / > <
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+      : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
 };
