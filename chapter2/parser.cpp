9ab2762a86025b6282b123010f06a608a65484eb
andreluizrcn <andreluizrodriguescastro@gmail.com>
2026-06-17 13:56:06 -0300

    added Caller class for expression to AST

--- chapter2/parser.cpp
+++ chapter2/parser.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 class ExprAST {
 public:
@@ -16,6 +17,7 @@ public:
   NumberExprAST(double V) : Val(V) {} // constructor + init
 };
 
+/// Expression class for referencing a var, like 'a,b,c.'
 class VariableExprAST : public ExprAST {
   std::string Name;
 
@@ -25,6 +27,7 @@ public:
   VariableExprAST(const std::string &Name) : Name(Name) {}
 };
 
+/// Expression class for a bin operation
 class BinaryExprAST : public ExprAST {
   char Op; // + - * / > <
   std::unique_ptr<ExprAST> LHS, RHS;
@@ -33,4 +36,15 @@ public:
   BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
                 std::unique_ptr<ExprAST> RHS)
       : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+/// Expression class for function calls
+class CallExprAST : public ExprAST {
+  std::string Calee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Calee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+      : Calee(Calee), Args(std::move(Args)) {}
 };
