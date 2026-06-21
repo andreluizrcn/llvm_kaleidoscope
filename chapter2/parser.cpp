a993a6ecd33245b7f943474f3ea2148d3a42e831
andreluizrcn <andreluizrodriguescastro@gmail.com>
2026-06-20 22:04:06 -0300

    Parser base structure formed - added Prototype and Function AST to capture function extracted from lexer

--- chapter2/parser.cpp
+++ chapter2/parser.cpp
@@ -1,5 +1,6 @@
 /* ============== CHAPTER 2 - PARSER =================*/
 
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <utility>
@@ -38,13 +39,38 @@ public:
       : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
 };
 
-/// Expression class for function calls
+/* Expression class for function calls */
 class CallExprAST : public ExprAST {
   std::string Calee;
   std::vector<std::unique_ptr<ExprAST>> Args;
 
 public:
+  // vector to unique ptr to expr nodes
   CallExprAST(const std::string &Calee,
               std::vector<std::unique_ptr<ExprAST>> Args)
       : Calee(Calee), Args(std::move(Args)) {}
+};
+
+/* This class represents the "prototype" for a function,
+ which captures its name, and its argument names (thus implicitly the number
+ of arguments the function takes). */
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+      : Name(Name), Args(std::move(Args)) {}
+  const std::string &getName() const { return Name; }
+};
+
+/* This class represents a function definition itself. */
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+      : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
